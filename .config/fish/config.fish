2963ef8e5bc162e3d66cd46d865eee0338d71f7a
ifuad <i.fuad.tm@gmail.com>
2021-12-28 21:11:41 +0400

    refactor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -100,7 +100,7 @@ function removeAllSnapshotsExceptLast
 end
 
 
-set -g -x PATH $PATH /home/qaqulya/.local/bin /home/qaqulya/.npm-global/bin /home/qaqulya/.dotnet 
+set -g -x PATH $PATH /home/qaqulya/.local/bin /home/qaqulya/.npm-global/bin /home/qaqulya/.dotnet /opt/mssql-tools/bin 
 set g -x XDG_DATA_DIRS /var/lib/flatpak/exports:/share/home/qaqulya/.local/share/flatpak/exports/share:/var/lib/snapd/desktop/:$XDG_DATA_DIRS
 
 #/usr/share/dotnet
@@ -111,12 +111,17 @@ set -g -x QT_AUTO_SCREEN_SCALE_FACTOR 0
 set -g -x QT_SCALE_FACTOR 1
 
 #alias nvim /home/qaqulya/.local/bin/neovim
-function vim
-    /home/qaqulya/.local/bin/neovim $argv
-end
-function nvim
-    /home/qaqulya/.local/bin/neovim $argv
-end
+#function vim
+#    /home/qaqulya/.local/bin/neovim $argv
+#end
+#function nvim
+#    /home/qaqulya/.local/bin/neovim $argv
+#end
+alias rm="echo Use 'del' or 'deldir', or the full path i.e. '/bin/rm'"
+#alias del="rmtrash"
+#alias deldir="rmdirtrash"
+alias del="trash-put"
+alias deldir="trash-put -d"
 function lastsnap 
 	set --local last_snap_num (sudo snapper list --columns number | grep "*" | tr -d "*")
 	if test -z $last_snap_num
@@ -128,10 +133,52 @@ end
 function trix
 	sudo /usr/sbin/transactional-update -c (lastsnap) $argv
 end
-function gitacp
-	git add .;git commit -am "refactor";git push origin main
+function gpacp
+	git pull; git add .;git commit -am "refactor";git push origin main
 end
 set -g -x EDITOR "neovim"
 set -g -x VISUAL "neovim" 
 #set -g -x XDG_DATA_DIRS $XDG_DATA_DIRS/var/lib/flatpak/exports/share /home/qaqulya/.local/share/flatpak/exports/share
 set -g -x DOTNET_CLI_TELEMETRY_OPTOUT 1
+
+trap "tmux kill-session" CONT 
+#trap 'tmux kill-session' 0
+#
+#trap -- 'echo 0 >> log.txt' EXIT
+#trap -- 'echo 1 >> log.txt' HUP
+#trap -- 'echo 2 >> log.txt' INT
+#trap -- 'echo 3 >> log.txt' QUIT
+#trap -- 'echo 4 >> log.txt' ILL
+#trap -- 'echo 5 >> log.txt' TRAP
+#trap -- 'echo 6 >> log.txt' ABRT
+###trap -- 'echo 7 >> log.txt' EMT
+#trap -- 'echo 8 >> log.txt' FPE
+#trap -- 'echo 9 >> log.txt' KILL
+#trap -- 'echo 10 >> log.txt' BUS
+#trap -- 'echo 11 >> log.txt' SEGV
+#trap -- 'echo 12 >> log.txt' SYS
+#trap -- 'echo 13 >> log.txt' PIPE
+#trap -- 'echo 14 >> log.txt' ALRM
+#trap -- 'echo 15 >> log.txt' TERM
+#trap -- 'echo 16 >> log.txt' URG
+#trap -- 'echo 17 >> log.txt' STOP
+#trap -- 'echo 19 >> log.txt && tmux kill-session' CONT
+#trap -- 'echo 20 >> log.txt' CHLD
+#trap -- 'echo 23 >> log.txt' IO
+#trap -- 'echo 24 >> log.txt' XCPU
+#trap -- 'echo 25 >> log.txt' XFSZ
+#trap -- 'echo 26 >> log.txt' VTALRM
+#trap -- 'echo 27 >> log.txt' PROF
+#trap -- 'echo 28 >> log.txt' WINCH
+###trap -- 'echo 29 >> log.txt' INFO
+#trap -- 'echo 30 >> log.txt' USR1
+#trap -- 'echo 31 >> log.txt' USR2
+#
+#
+###trap -- 'echo 32 >> log.txt' STKFLT
+#trap -- 'echo 33 >> log.txt' TSTP
+#trap -- 'echo 34 >> log.txt' TTIN
+#trap -- 'echo 35 >> log.txt' TTOU
+###trap -- 'echo 36 >> log.txt' POLL
+#trap -- 'echo 37 >> log.txt' PWR
+#
